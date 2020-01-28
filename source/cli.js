3394eea576fee21a5bb7f86dff7afc0ab5a49887
Dennis Muensterer <dennismuensterer@gmail.com>
2020-01-28 10:53:49 +0100

    Fix XO errors

--- source/cli.js
+++ source/cli.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
-'use strict'
-const path = require('path')
-const meow = require('meow')
-const termImg = require('term-img')
-const terminalImage = require('terminal-image')
-const importJsx = require('import-jsx')
-const React = require('react')
-const {render} = require('ink')
+'use strict';
+// Const path = require('path');
+const meow = require('meow');
+// Const termImg = require('term-img');
+// const terminalImage = require('terminal-image');
+const importJsx = require('import-jsx');
+const React = require('react');
+const {render} = require('ink');
 
-const ui = importJsx('./ui')
+const ui = importJsx('./ui');
 
 meow(`
 	Usage
 	  $ dnnsmnstrr
-`)
+`);
 
-// const fallback = async () => {
+// Const fallback = async () => {
 // 	const image = await terminalImage.file(path.join(__dirname, 'avatar-fallback.png'));
 // 	console.log(image);
 // };
 
 // termImg(path.join(__dirname, 'avatar.png'), {fallback});
 
-render(React.createElement(ui))
+render(React.createElement(ui));
